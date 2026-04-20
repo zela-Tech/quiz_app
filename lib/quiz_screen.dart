c18999efdafa7c88f6a96acd5c3b8ed696394906
Adom Z <zelalemadom@gmail.com>
2026-04-19 20:34:31 -0400

    feat(ui): add basic QuizScreen scaffold and route from main.dart

---
+++ lib/quiz_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class QuizScreen extends StatefulWidget {
+  const QuizScreen({super.key});
+
+  @override
+  State<QuizScreen> createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child:Text('Quiz'),
+      ),
+    );
+  }
+}
